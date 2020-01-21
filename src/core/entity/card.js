cc5497deb38ebe6418a7248d445c8a2a16a84fc4
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-01-21 22:58:30 +0700

    Remove id props from card & desk.

--- src/core/entity/card.js
+++ src/core/entity/card.js
@@ -1,7 +1,14 @@
 export default class Card {
     
-    Card(_id, _front, _back, _type, _ease, _interval, _lastReviewTime) {
-        this.id = _id;
+    /**
+     * @param {string} _front Unique question, also used to generate id.
+     * @param {string} _back Answer.
+     * @param {string} _type
+     * @param {number} _ease
+     * @param {number} _interval 
+     * @param {Date} _lastReviewTime 
+     */
+    Card(_front, _back, _type, _ease, _interval, _lastReviewTime) {
         this.front = _front;
         this.type = _type;
         this.ease = _ease;
--- src/core/entity/desk.js
+++ src/core/entity/desk.js
@@ -1,7 +1,11 @@
 export default class Desk {
-    
-    Desk(_id, _name, _easeBonus, _intervalModifier, _cardIds) {
-        this.id = _id;
+    /**
+     * @param {string} _name Unique, used to generate id.
+     * @param {number} _easeBonus Bonus when user get an "easy" answer.
+     * @param {number} _intervalModifier 
+     * @param {string} _cardIds Ids of all the cards in this desk.
+     */
+    Desk(_name, _easeBonus, _intervalModifier, _cardIds) {
         this.name = _name;
         this.easeBonus = _easeBonus;
         this.intervalModifier = _intervalModifier;
